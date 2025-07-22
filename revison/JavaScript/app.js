d4a1e469a18e63d58abdeb31bf7aaa17615f90d1
Shivratn Saharan <shivratnsaharan@gmail.com>
2025-07-22 17:32:01 +0530

    22/07/25

---
+++ revison/JavaScript/app.js
@@ -0,0 +1,6 @@
+// alert("hello ");
+// let a = prompt("Enter Your Name : ");
+console.log("Enter Your Namber1 : ");
+console.log("Enter Your Namber2 : ");
+console.log("Enter Your Namber3 : ");
+console.log("Enter Your Namber4 : ");
