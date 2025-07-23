cfe62c91d6884e327dd26e06cb0c4432d4054732
Shivratn Saharan <shivratnsaharan@gmail.com>
2025-07-23 16:54:04 +0530

    23/07/2025

--- revison/JavaScript/app.js
+++ revison/JavaScript/app.js
@@ -1,6 +1,42 @@
 // alert("hello ");
 // let a = prompt("Enter Your Name : ");
+/*
 console.log("Enter Your Namber1 : ");
 console.log("Enter Your Namber2 : ");
 console.log("Enter Your Namber3 : ");
 console.log("Enter Your Namber4 : ");
+
+age = 17;
+if(age > 10 && age < 20) {
+    console.log("y");
+}
+else{
+    console.log("n");
+}
+
+
+let a = 1;
+for (let i = 0; i < 20; i++) {
+    console.log(a+i);
+    
+}
+
+*/
+/*
+let b = 1;
+for(let i = 0; i < 12; i++) {
+     console.log(b+i);
+}
+*/
+/*
+
+function ok(name) {
+    console.log(name + " you are good");
+    console.log(name + " you are very good");
+    console.log(name + " you are best");
+    console.log(name + " you are nice");
+}
+ok("Shivratn");
+
+*/
+
